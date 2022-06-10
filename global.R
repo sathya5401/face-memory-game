b4b74eb93814810019d5a3fefc8c0bdeb395c096
sathya5401 <76908303+sathya5401@users.noreply.github.com>
2022-06-10 10:47:45 +0800

    Update global.R

--- global.R
+++ global.R
@@ -1,14 +1,4 @@
 
-#  ------------------------------------------------------------------------
-#
-# Title : Memory Hex - Global
-#    By : dreamRs
-#  Date : 2019-02-06
-#    
-#  ------------------------------------------------------------------------
-
-
-
 # Packages ----------------------------------------------------------------
 
 library("shiny")
@@ -35,5 +25,6 @@ source("functions/utils.R")
 n_hex <- 10
 
 shareurl <- "https://twitter.com/intent/tweet?text=I%20found%20all%20matching%20Hex%20in%20{time}%20seconds%20!%20And%20you%20?%20%23rstats%20%23shiny%20Play%20here:&url=https://dreamrs.shinyapps.io/memory-hex"
+
 
 
